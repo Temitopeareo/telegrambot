2cc6c61692bc4c307f277ddcd1f6a5ae626ffacf
Temitopeareo <132304443+Temitopeareo@users.noreply.github.com>
2025-03-14 08:40:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // This is the main entry point for Vercel
 const app = require("./bot.js")
 
+// Add a simple root route for testing
+app.get("/", (req, res) => {
+  res.status(200).send("Bot server is running. Go to /api/debug for more information.")
+})
+
 // Export the Express app for Vercel
 module.exports = app
+
